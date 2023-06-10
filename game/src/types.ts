457910de7702031c00a43b9fe5d0d3a7d58e7f27
Pedro Carlos Martinez <mpedroc90@gmail.com>
2023-06-10 16:04:18 +0200

    mend

--- game/src/types.ts
+++ game/src/types.ts
@@ -2,9 +2,6 @@ import { Game } from "./Game";
 import { Piece } from "./pieces/Piece";
 
 
-
-
-
 /** Represent the cell coordenates in the board */
 export type Cell = {
     id: String,
@@ -36,36 +33,3 @@ export enum Color {
 export type Rule = (piece:Piece, game: Game) => boolean
 
 
-/** Rules  */
-
-/**
- * 
- * @param piece 
- * @param _ 
- * @returns 
- */
-export const PieceInSecondRow: Rule = (piece:Piece, _:Game): boolean => {
-    return (piece.color == Color.White && piece.position.row == 1)
-        || (piece.color == Color.Black && piece.position.row == 6);
-}
-
-
-/**
- * 
- * @param piece 
- * @param _ 
- * @returns 
- */
-export const PieceInFifthRow: Rule = (piece:Piece, _:Game): boolean => {
-    return (piece.color == Color.White && piece.position.row == 5)
-        || (piece.color == Color.Black && piece.position.row == 4);
-}
-
-/**
- * 
- * @param piece 
- * @param game 
- * @returns 
- */
-
-export const KingIsInCheck: Rule = ( piece:Piece, game: Game ) => true
